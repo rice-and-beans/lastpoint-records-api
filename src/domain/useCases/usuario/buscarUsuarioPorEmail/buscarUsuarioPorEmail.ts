8715876e2f67397b93a19974f9889a299c25d66e
Breno Augusto Comandolli <brenoaugustocomandolli@gmail.com>
2022-06-16 21:07:02 -0300

    #0011 - Agora

---
+++ src/domain/useCases/usuario/buscarUsuarioPorEmail/buscarUsuarioPorEmail.ts
@@ -0,0 +1,21 @@
+import { Usuario } from "../../../../data/entities/usuario";
+import { IUsuarioRepository } from "../../../repositories/usuarioRepository";
+import { IAtualizarUsuarioRequestDTO } from "../../../model/usuarioDTO";
+import { ValidacaoBase } from "../../../validations/ValidacaoBase";
+
+export class AtualizarUsuarioUseCase {
+
+    constructor(
+        private usuarioRepository: IUsuarioRepository,
+        private validaParamObrigatorio: ValidacaoBase,
+    ){}
+
+    async execute(data: IAtualizarUsuarioRequestDTO){
+        const dadosValidacao = new Map<Object, string>([
+            [data.codigo, "codigo"]
+        ]);
+        await this.validaParamObrigatorio.valida(dadosValidacao);
+        return await this.usuarioRepository.buscarPorEmail(email);
+    }
+
+}
